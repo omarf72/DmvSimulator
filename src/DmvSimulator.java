ba6d3cf1c066e4291f0c9c9791997cbf33f20031
github user name <github email>
2024-03-05 15:45:02 -0500

    Welcoming the user

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,3 +1,39 @@
+import java.util.Random;
+import java.security.SecureRandom;
+
 public class DmvSimulator {
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to the DMV ");
+
+        SecureRandom ran= new SecureRandom();
+        
+        int num=ran.nextInt(100)+1;
+
+
+        System.out.println("Your number is "+num+" please wait until it is called.");
+
+        for (int i = num + 1; ; i++) {
+            if (i > 100) {
+                i = 1; // Wrap around if we reach 100
+
+
+            }
+
+            System.out.println("Now serving number " + i + "...");
+            System.out.println();
+            
+           
+            if (i == num) {
+                System.out.println("Your number (" + num + ") is now being served. Please proceed to the counter.");
+                System.out.println();
+                break; // Exit the loop once user's number is called
+            }
+
+
+    }
+    System.out.println(" You dont have required paperwork ");
+
+    }
 }
+
