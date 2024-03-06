0403813c005636f80d674edb6002de0cdfd21bdb
Philip <panora@quinnipiac.edu>
2024-03-06 17:17:02 -0500

    evil laugh

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -32,7 +32,7 @@ public class DmvSimulator {
 
 
     }
-    System.out.println("This isn't even close to the right stuff! Get out of here!");
+    System.out.println("This isn't even close to the right stuff! Get out of here! MUAHAHAHA");
 
     }
 }
